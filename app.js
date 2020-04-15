e367947b4b3249bf2594539a53f7003c347b6a57
Eugene Prokopiev <enp@itx.ru>
2020-04-15 15:23:54 +0300

    persistence via elasticsearch

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express       = require('express'),
+const elastic       = require('@elastic/elasticsearch'),
+      express       = require('express'),
       morgan        = require('morgan'),
       passport      = require('passport'),
+      bodyParser    = require('body-parser'),
       cookieParser  = require('cookie-parser'),
       cookieSession = require('cookie-session'),
       auth          = require('./auth'),
       config        = require('./config')
+
+global.es = new elastic.Client(config.elastic)
 
 const app = express()
 
@@ -19,6 +23,8 @@ app.use(cookieSession({
     maxAge: 24 * 60 * 60 * 1000
 }))
 app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
 app.get('/auth/logout', (req, res) => {
     req.logout()
@@ -40,7 +46,60 @@ app.get('/auth/session', (req, res) => {
     res.json(req.session)
 })
 
-//app.route('/api/:action').get((req,res) => { res.json({ action: req.params.action, session: req.session }) })
+app.all('/data/:id?', async (req, res) => {
+    try {
+        if (req.method != 'GET' && !req.session.passport) {
+            res.status(403).set('Content-Type', 'text/plain').send('Access forbidden')
+        }
+        switch (req.method) {
+            case 'POST': {
+                const data = await es.index({
+                    index: config.elastic.index.data,
+                    id: req.params.id,
+                    body: Object.assign({ user: { name: req.session.passport.user.profile.displayName, time: new Date() }}, req.body),
+                    refresh: 'wait_for'
+                })
+                res.json({ id: data.body._id, result: data.body.result })
+                break
+            }
+            case 'PUT': {
+                req.body.user = { name: req.session.passport.user.profile.displayName, time: new Date() }
+                const data = await es.update({
+                    index: config.elastic.index.data,
+                    id: req.params.id,
+                    body: {
+                        doc: Object.assign({ user: { name: req.session.passport.user.profile.displayName, time: new Date() }}, req.body),
+                        doc_as_upsert: true
+                    },
+                    refresh: 'wait_for'
+                })
+                res.json({ id: data.body._id, result: data.body.result })
+                break
+            }
+            case 'GET': {
+                if (req.params.id) {
+                    const data = await es.get({ index: config.elastic.index.data, id: req.params.id })
+                    res.json(data.body._source)
+                } else {
+                    const data = await es.search({ index: config.elastic.index.data, q: req.query.q, size: req.query.size })
+                    res.json(data.body.hits.hits.reduce(function(map, hit) {
+                        map[hit._id] = hit._source
+                        return map
+                    }, {}))
+                }
+                break
+            }
+            case 'DELETE': {
+                const data = await es.delete({ index: config.elastic.index.data, id: req.params.id, refresh: 'wait_for' })
+                res.json({ id: data.body._id, result: data.body.result })
+                break
+            }
+        }
+    } catch(error) {
+        //console.log(error.toString())
+        res.status(500).set('Content-Type', 'text/plain').send(error.stack)
+    }
+})
 
 app.listen(config.daemon.port, config.daemon.host)
 
--- auth.js
+++ auth.js
@@ -2,13 +2,23 @@ const config = require('./config')
 
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 
-module.exports = function (passport) {
-    passport.serializeUser((user, done) => {
-        console.log({ action: 'serialize', user: user })
+const cleanup = (user) => {
+    const data = JSON.parse(JSON.stringify(user))
+    delete data.profile._raw
+    delete data.profile._json
+    return data
+}
+
+module.exports = (passport) => {
+    passport.serializeUser(async (user, done) => {
+        const data = cleanup(user)
+        const id = `${data.profile.provider}-${data.profile.id}`
+        console.log({ action: 'serialize', user: data })
+        await es.index({ index: config.elastic.index.auth, id: id, body: data })
         done(null, user)
     })
     passport.deserializeUser((user, done) => {
-        console.log({ action: 'deserialize', user: user })
+        console.log({ action: 'deserialize', user: cleanup(user) })
         done(null, user)
     })
     passport.use(new GoogleStrategy({
